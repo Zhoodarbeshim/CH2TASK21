8482a0eac44771fdd17bd04d7ce745dda4ba9cde
Zhoodarbeshim <zh.bektursunov@yandex.com>
2019-12-30 09:24:33 +0600

    twenty first commit

---
+++ task21.py
@@ -0,0 +1,11 @@
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter a number: "))
+# num3 = int(input("Enter a number: "))
+# print(num1+num2+num3)
+
+def sum_(num1, num2, num3):
+    return num1+num2+num3
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+num3 = int(input("Enter a number: "))
+print(sum_(num1, num2, num3))  
